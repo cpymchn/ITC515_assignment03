3e01c461a061a5df98597fbfe2d7cb4ecf2f68ad
mb <edu.markb@gmail.com>
2015-11-10 22:41:05 -0500

    Unit test creation

---
+++ referenceImplementation/src/tests/library/entities/TestBook.java
@@ -0,0 +1,119 @@
+package tests.library.entities;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.ILoan;
+import library.interfaces.entities.IMember;
+
+import library.entities.Book;
+
+public class TestBook
+{
+  @Test
+  public void testBook()
+  {
+    fail("Not yet implemented");
+  }
+
+
+
+  @Test
+  public void testBorrow()
+  {
+    fail("Not yet implemented");
+  }
+
+
+
+  @Test
+  public void testGetLoan()
+  {
+    fail("Not yet implemented");
+  }
+
+
+
+  @Test
+  public void testReturnBook()
+  {
+    fail("Not yet implemented");
+  }
+
+
+
+  @Test
+  public void testLose()
+  {
+    fail("Not yet implemented");
+  }
+
+
+
+  @Test
+  public void testRepair()
+  {
+    fail("Not yet implemented");
+  }
+
+
+
+  @Test
+  public void testDispose()
+  {
+    fail("Not yet implemented");
+  }
+
+
+
+  @Test
+  public void testGetState()
+  {
+    fail("Not yet implemented");
+  }
+
+
+
+  @Test
+  public void testGetAuthor()
+  {
+    fail("Not yet implemented");
+  }
+
+
+
+  @Test
+  public void testGetTitle()
+  {
+    fail("Not yet implemented");
+  }
+
+
+
+  @Test
+  public void testGetCallNumber()
+  {
+    fail("Not yet implemented");
+  }
+
+
+
+  @Test
+  public void testGetID()
+  {
+    fail("Not yet implemented");
+  }
+
+
+
+  @Test
+  public void testToString()
+  {
+    fail("Not yet implemented");
+  }
+}
